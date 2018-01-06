09b806fd7be05601f9e117c3e1882dbc3f214b4a
鬼谣 <lihua.wang@alibaba-inc.com>
2018-01-06 20:21:39 +0800

    [add] 增加潜入新闻iframe

--- lbs4life/routes/index.js
+++ lbs4life/routes/index.js
@@ -3,7 +3,19 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+  res.render('index', {
+    menu: 'menu-weizhi-kuaiji'
+  });
+});
+
+router.get('/weizhi/kuaiji.html', function(req, res, next) {
+  res.render('index', {
+    menu: 'menu-weizhi-kuaiji'
+  });
+});
+
+router.get('/weizhi/fangjia.html', function(req, res, next) {
+  res.render('weizhi/fangjia', {menu: 'menu-weizhi-fangjia'});
 });
 
 module.exports = router;
